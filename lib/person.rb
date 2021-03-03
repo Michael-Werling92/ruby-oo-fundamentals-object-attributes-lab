6496b6ecd5a887f8dbcfe5d680087c70714fa343
Michael <mfw6mw@virginia.edu>
2021-03-03 10:45:21 -0500

    commit tree df4be3943bd59549248bb2a6eb8dd77673de9bcd

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name=(new_name)
+        @name = new_name
+    end
+    def name
+        @name
+    end
+    def job=(current_job)
+        @job = current_job
+    end
+    def job
+        @job
+    end
+end
